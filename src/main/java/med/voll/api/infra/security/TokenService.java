0f5a1f9097c11dda3f22b7c2cfc7ef36ef6661c3
andylorrayne <alorraynefigueiredo@gmail.com>
2023-10-01 15:19:27 -0300

    jwt configuração 2

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
@@ -14,11 +15,14 @@ import med.voll.api.domain.usuario.Usuario;
 
 @Service
 public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
 
     public String gerarToken(Usuario usuario){
         /* o codigo foi fornecido no gitHub da biblioteca, apenas foi feita algumas alterações */
+        System.out.println(secret);
         try {
-            var algoritmo = Algorithm.HMAC256("senha");
+            var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
                 .withIssuer("API Voll.med")
                 .withSubject(usuario.getLogin())
