7ca9a6628b663b35752680ce42aa7ccfeeb0f1fc
andylorrayne <alorraynefigueiredo@gmail.com>
2023-10-01 17:48:56 -0300

    mudando metodo SecurityFiterChain spring 3

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -23,12 +23,14 @@ public class SecurityConfigurations {
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(c -> c.disable())
-                .cors(c -> c.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/login").permitAll().anyRequest().authenticated())
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+        return http.csrf(csrf -> csrf.disable())
+        .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(req -> {
+            req.requestMatchers(HttpMethod.POST, "/login").permitAll();
+            req.anyRequest().authenticated();
+        })
+        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+        .build();
     }
 
     @Bean
